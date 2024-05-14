0f53686c44d5bdf3df79b690b467a2b8491a0edb
Hayoon Lim / 임하윤 <hayoon603@gmail.com>
2024-05-14 16:34:14 +0900

    feat: add play btn in menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // 1 for "Game" scene
+        SceneManager.LoadSceneAsync(1);
+    }
+}
