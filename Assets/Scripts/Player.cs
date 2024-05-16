6e8c60c210021c8ede24d433b325a2e5f7fdb4d7
Moumee <zzuzzu1009@naver.com>
2024-05-16 23:08:57 +0900

    Reapply "Merge branch 'main' of https://github.com/yoonyLim/ProjectProPotato"
    This reverts commit 1911860f08fa27f9afcdf2e6023a93d1ba29e428.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Animator potatoAnimator;
+
     public bool isRight;
+
+    private Vector3 rightPos = Vector3.right * 5 + Vector3.up * 0.67f;
+    private Vector3 leftPos = Vector3.left * 5 + Vector3.up * 0.67f;
+
 
     // Start is called before the first frame update
     void Awake()
     {
         isRight = true;
+        potatoAnimator = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isRight)
-            transform.position = Vector3.right*5+Vector3.up*2.5f;
-        else
-            transform.position = Vector3.left*5 + Vector3.up * 2.5f;
+        transform.position = Vector3.MoveTowards(transform.position, (isRight ? rightPos : leftPos), 0.1f);
+        //if (isRight)
+        //    transform.position = Vector3.right*5+Vector3.up*0.67f;
+        //else
+        //    transform.position = Vector3.left*5 + Vector3.up * 0.67f;
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            //potatoAnimator.SetBool("isJump", true);
             isRight = !isRight;
         }
     }
