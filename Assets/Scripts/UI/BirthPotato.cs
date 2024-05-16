f2342d2a5e36b9b70f1195972cac1bec110cfeef
Hayoon Lim / 임하윤 <hayoon603@gmail.com>
2024-05-16 23:38:43 +0900

    feat: modify main menu

---
+++ Assets/Scripts/UI/BirthPotato.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirthPotato : MonoBehaviour
+{
+    private void TossPotato()
+    {
+        Random.InitState((int)System.DateTime.Now.Ticks);
+
+        GameObject potato = PotatoPool.pool.GetPooledPotato();
+
+        if (potato != null)
+        {
+            potato.transform.position = new Vector3(Random.Range(-960, 960), 0.0f, 0.0f);
+            potato.SetActive(true);
+        }
+    }
+
+    void Start()
+    {
+        InvokeRepeating("TossPotato", 0.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/BlowGaugeController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class BlowGaugeController : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI percentTxt = null;
+    [SerializeField]
+    private Slider blowGauge = null;
+
+    private float unitPercent = 1.0f;
+
+    public void SetPercentTxt(float val)
+    {
+        float res = val * unitPercent;
+        percentTxt.text = res.ToString("0") + "%";
+    }
+
+    void DecreasePercent()
+    {
+        if (blowGauge.value > 0.0f && blowGauge.value < 100.0f)
+        {
+            blowGauge.value -= 1.0f;
+        }
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("DecreasePercent", 0.0f, 0.3f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            blowGauge.value += 5.0f;
+        }
+
+        if (blowGauge.value == 100.0f)
+        {
+            SceneManager.LoadSceneAsync(1);
+        }
+    }
+}
