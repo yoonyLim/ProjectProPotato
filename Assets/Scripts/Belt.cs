1ded55246b331559a38ad8c4b079daa3bf4e364f
parkyoungwoo <snake010807@khu.ac.kr>
2024-05-16 18:41:42 +0900

    add Potato

--- Assets/Scripts/Belt.cs
+++ Assets/Scripts/Belt.cs
@@ -14,8 +14,8 @@ public class Belt : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (transform.position.z > 84)
-            transform.position = new Vector3(0, 0, -80);
+        if (transform.position.z >= 82)
+            transform.position = new Vector3(0, 0, -84);
         transform.position += Vector3.forward * speed * Time.fixedDeltaTime;
         
     }
