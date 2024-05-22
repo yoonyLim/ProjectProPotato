48a1d3ebdb8f903b99b4e1e9ad22ecd4f2fd5a46
DESKTOP-VCOIP79\USER <snake010807@khu.ac.kr>
2024-05-23 03:56:30 +0900

    점프 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,44 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using UnityEditor.Experimental.GraphView;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] GenericObserver<int> Lives = new GenericObserver<int>(3);
 
     Animator potatoAnimator;
+    Rigidbody rigid;
 
-    public bool isRight;
+    public CinemachineVirtualCamera virCam;
+    CinemachineBasicMultiChannelPerlin noiseParam;
+    
 
-    private Vector3 rightPos = Vector3.right * 5 + Vector3.up * 0.67f;
-    private Vector3 leftPos = Vector3.left * 5 + Vector3.up * 0.67f;
+    public bool isRight, isDodge;
+    public float speed = 20;
+
+    private Vector3 rightPos = Vector3.right * 5 + Vector3.up * 0.5f;
+    private Vector3 leftPos = Vector3.left * 5 + Vector3.up * 0.5f;
 
 
     // Start is called before the first frame update
     void Awake()
     {
         Lives.Invoke();
+
         isRight = true;
+        isDodge = false;
+
         potatoAnimator = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        noiseParam = virCam.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+
+        transform.position = rightPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, (isRight ? rightPos : leftPos), 0.1f);
-        //if (isRight)
-        //    transform.position = Vector3.right*5+Vector3.up*0.67f;
-        //else
-        //    transform.position = Vector3.left*5 + Vector3.up * 0.67f;
+        //transform.Translate(Vector3.MoveTowards(transform.position, (isRight?rightPos:leftPos), 0.1f).x, 0.5f, 0);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (isRight && transform.position.x < rightPos.x)
+        {
+            transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+        else if (!isRight && transform.position.x > leftPos.x)
+        {
+            transform.position += Vector3.left * Time.deltaTime * speed;
+        }
+    
+        
+
+        if (Input.GetKeyDown(KeyCode.Space)&&!isDodge)
         {
             //potatoAnimator.SetBool("isJump", true);
-            isRight = !isRight;
+            
+            StartCoroutine(DodgeTime());
         }
 
         // If player is hit by a knife or a roller
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
-            Lives.Value--;
+        {
+            StartCoroutine(Hit());
+        }
+    }
+
+    IEnumerator DodgeTime()
+    {
+        isRight = !isRight;
+        isDodge = true;
+        rigid.velocity += Vector3.up * 20f;
+        yield return new WaitForSeconds(0.5f);
+        rigid.velocity += Vector3.down * 30f;
+        yield return new WaitForSeconds(0.4f);
+        isDodge = false;
+    }
+
+    IEnumerator Hit()
+    {
+        noiseParam.m_AmplitudeGain = 12;
+        Lives.Value--;
+        yield return new WaitForSeconds(0.5f);
+        noiseParam.m_AmplitudeGain = 0;
     }
 }
